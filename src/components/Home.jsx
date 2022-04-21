70e08ac202dca40538e624b0495de5f774c517dc
Anurag Singh <sa.anuragsingh@gmail.com>
2022-04-22 01:04:43 +0530

    webcam working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
+import Webcam from "react-webcam";
 import ImageSlider from "./ImageSlider";
+
+const videoConstraints = {
+  facingMode: "user",
+};
 
 const Home = () => {
   const handleSearch = (event) => {
@@ -10,9 +18,14 @@ const Home = () => {
     // fetch search result
   };
 
+  const fetchMusic = (image) => {
+    //AXIOS REQUEST HERE
+    console.log(image);
+  };
+
   return (
     <>
-     {/* <Box sx={{display: "flex", flexDirection: "column"}}> */}
+      {/* <Box sx={{display: "flex", flexDirection: "column"}}> */}
       <TextField
         fullWidth
         id="search"
@@ -23,6 +36,54 @@ const Home = () => {
       />
 
       <ImageSlider />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Box
+            sx={{
+              height: 300,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Webcam
+            audio={false}
+            width={400}
+            videoConstraints={videoConstraints}
+            mirrored={true}
+          >
+            {({ getScreenshot }) => (
+              <Grid
+                container
+                pt={1}
+                pb={1}
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={5}
+              >
+                <Grid item xs={7}>
+                  <Typography component="div">You are Happy!</Typography>
+                </Grid>
+                <Grid item xs={5}>
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      const imageSrc = getScreenshot();
+                      fetchMusic(imageSrc);
+                    }}
+                  >
+                    Analyse Mood
+                  </Button>
+                </Grid>
+              </Grid>
+            )}
+          </Webcam>
+        </Grid>
+      </Grid>
       {/* <Box compo sx={{flex: 1, bgcolor: "#ff0000"}} /> */}
     </>
   );
